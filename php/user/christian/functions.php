9e2035180019052153821ed249d38ce8aa3f67a5
Christian <christian.wagner1204@gmail.com>
2025-10-07 10:22:32 +0200

    Erweiterung um Funktionen

---
+++ php/user/christian/functions.php
@@ -0,0 +1,32 @@
+<?php
+function save($user, $content)
+{
+    $file = fopen('shouts.txt', 'a');
+        switch (strtolower($user)) {
+            case 'christian':
+                $bgColor = '#95dea8';
+                break;
+            case 'melina':
+                $bgColor = '#ca6fd6';
+                break;
+            case 'lars':
+                $bgColor = '#92f0ed';
+                break;
+            default:
+                $bgColor = 'beige';
+                break;
+        }
+        if ($file) {
+            $zeile = ' 
+        <table cellspacing="2" align="center" width="350"> 
+          <tr> 
+            <td bgcolor="' . $bgColor . '">' . $user . '</td> 
+            <td bgcolor="' . $bgColor . '">' . $content . '</td> 
+          </tr> 
+        </table>';
+            fwrite($file, $zeile);
+            fclose($file);
+        } else {
+            echo 'Datei nicht schreibbar!';
+        }
+}
--- php/user/christian/shoutbox.php
+++ php/user/christian/shoutbox.php
@@ -1,72 +1,50 @@
 <!doctype html>
 <html lang="de">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>ShoutBox</title>
 </head>
 <body>
+    <br />
+    <form action="shoutbox.php" method="post">
+        <table align="center" width="350">
+            <tr>
+                <td>Name:</td>
+                <td><input type="text" name="user" value="" /></td>
+            </tr>
+            <tr>
+                <td>Inhalt:</td>
+                <td><input type="text" name="content" value="" /></td>
+            </tr>
+            <tr>
+                <td colspan="2" align="center">
+                    <input type="submit" name="shout" value="rufen" />
+                </td>
+            </tr>
+        </table>
+    </form>
 <?php
-$file = fopen('shouts.txt', 'a');
-if( !empty($_REQUEST['user']) && !empty ($_REQUEST['content']) ) {
-    switch (strtolower($_REQUEST['user'])) {
-        case 'christian':
-            $bgColor = '#95dea8';
-            break;
-        case 'melina':
-            $bgColor = '#ca6fd6';
-            break;
-        case 'lars':
-            $bgColor = '#92f0ed';
-            break;
-        default:
-            $bgColor = 'beige';
-            break;
-    }
-    if ($file){
-        $zeile =  ' 
-        <table cellspacing="2" align="center" width="350"> 
-          <tr> 
-            <td bgcolor="'.$bgColor.'">'.$_REQUEST['user'].'</td> 
-            <td bgcolor="'.$bgColor.'">'.$_REQUEST['content'].'</td> 
-          </tr> 
-        </table>';
-        fwrite($file, $zeile);
-        fclose($file);
-    }else{
-        echo 'Datei nicht schreibbar!';
-    }
+// Die Datei mit der Funktion einbinden
+require_once 'functions.php';
+// Abfrage ob einer der Beiden Parameter gesetzt ist
+if (!empty($_REQUEST['user']) && !empty($_REQUEST['content'])) {
+    save($_REQUEST['user'], $_REQUEST['content']);
 }
 ?>
-<br />
-<form action="shoutbox.php" method="post">
-    <table align="center" width="350">
-        <tr>
-            <td>Name:</td>
-            <td><input type="text" name="user" value="" /></td>
-        </tr>
-        <tr>
-            <td>Inhalt:</td>
-            <td><input type="text" name="content" value="" /></td>
-        </tr>
-        <tr>
-            <td colspan="2" align="center">
-                <input type="submit" name="shout" value="rufen" />
-            </td>
-        </tr>
-    </table>
-</form>
 <?php
+// Ausgabe der Inhalte der Datei
 $file = fopen('shouts.txt', 'r');
-if ($file){
-    while (!feof($file)){
+if ($file) {
+    while (!feof($file)) {
         $zeile = fgets($file);
         echo $zeile;
     }
     fclose($file);
-}else {
+} else {
     echo 'Datei nicht lesbar!';
 }
 ?>
