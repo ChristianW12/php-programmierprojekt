b6ef2597633717474af7c180620722ede5670f3f
Christian <christian.wagner1204@gmail.com>
2025-10-12 22:28:43 +0200

    Erweiterungen an shouts.txt Datei

--- php/user/christian/Shout.php
+++ php/user/christian/Shout.php
@@ -4,7 +4,7 @@ class Shout{
     public static function shoutAusgeben(){
         // Ausgabe der Inhalte der Datei
         $file = fopen('shouts.txt', 'r');
-        if ($file) {
+        if ($file)              {
             while (!feof($file)) {
                 $zeile = fgets($file);
                 echo $zeile;
@@ -15,31 +15,32 @@ class Shout{
         }
     }
 
-    public static function save($user, $content)
-    {
+    public static function save($user, $content){
         $file = fopen('shouts.txt', 'a');
         switch (strtolower($user)) {
-            case 'christian':
-                $bgColor = '#95dea8';
-                break;
-            case 'melina':
-                $bgColor = '#ca6fd6';
-                break;
-            case 'lars':
-                $bgColor = '#92f0ed';
-                break;
-            default:
-                $bgColor = 'beige';
-                break;
+        case 'christian':
+            $bgColor = '#95dea8';
+            break;
+        case 'melina':
+            $bgColor = '#ca6fd6';
+            break;
+        case 'lars':
+            $bgColor = '#92f0ed';
+            break;       
+        case 'kathrin':
+            $bgColor = '#cf5be3';
+        default:
+            $bgColor = 'beige';
+            break;
         }
         if ($file) {
             $zeile = ' 
-        <table cellspacing="2" align="center" width="350"> 
-          <tr> 
-            <td bgcolor="' . $bgColor . '">' . $user . '</td> 
-            <td bgcolor="' . $bgColor . '">' . $content . '</td> 
-          </tr> 
-        </table>';
+                <table cellspacing="2" align="center" width="350"> 
+                <tr> 
+                <td bgcolor="' . $bgColor . '">' . $user . '</td> 
+                <td bgcolor="' . $bgColor . '">' . $content . '</td> 
+                </tr> 
+                </table>';
             fwrite($file, $zeile);
             fclose($file);
         } else {
