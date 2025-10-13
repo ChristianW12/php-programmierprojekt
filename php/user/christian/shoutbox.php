e82a1206e4f3ee66ec12900562f82c1060959fb9
Christian <christian.wagner1204@gmail.com>
2025-10-13 08:48:05 +0200

    Statische Methoden an OOP angepasst

--- php/user/christian/shoutbox.php
+++ php/user/christian/shoutbox.php
@@ -30,11 +30,12 @@
 <?php
 // Die Datei mit der Funktion einbinden
 require 'Shout.php';
+$shout = new Shout();
 // Abfrage ob einer der Beiden Parameter gesetzt ist
 if (!empty($_REQUEST['user']) && !empty($_REQUEST['content'])) {
-    Shout::save($_REQUEST['user'], $_REQUEST['content']);
+    $shout->save($_REQUEST['user'], $_REQUEST['content']);
 }
-Shout::shoutAusgeben();
+$shout->shoutAusgeben();
 ?>
 </body>
 </html>
