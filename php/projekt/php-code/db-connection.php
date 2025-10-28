3b26f754fb2e6f9553ea790ac5bd5fabb10c01b6
Christian <christian.wagner1204@gmail.com>
2025-10-28 17:55:50 +0100

    Profil erstellt mit Infos aus der DB

--- php/projekt/php-code/db-connection.php
+++ php/projekt/php-code/db-connection.php
@@ -1,11 +1,13 @@
 <?php
-
-$dsn = 'mysql:dbname=auktion;host=db;port=3306';
-
- try{
-        $db = new Db($dsn, 'root', '');
+require_once 'Db.php'; // Db-Klasse einbinden
+ function mitDBverbinden() {
+    # code...
+    $dsn = 'mysql:dbname=auktion;host=db;port=3306';
+    try{
+       return new Db($dsn, 'root', '');
     } catch (PDOException $e){
         echo 'Verbindungsfehler: ' . $e->getMessage();
         exit;
     }
+}
 ?>
--- php/projekt/profile.php
+++ php/projekt/profile.php
@@ -3,6 +3,20 @@ if (session_start() === PHP_SESSION_NONE){
     session_start();
 }
 
+if(!isset($_SESSION['loggedin']) ){
+    header('Location: index.php');
+    exit;
+}
+
+require 'php-code/db-connection.php';
+
+$db = mitDBverbinden();
+
+$stmt = $db->prepare("select* from users where mail = :mail");
+$stmt->execute([':mail' => $_SESSION['user_mail']]);
+// Daten vom User in einem Array speichern
+$user_from_db = $stmt->fetch(Db::FETCH_ASSOC);
+
 $verlinkungHomepage = 'index.php';
 ?>
 <!DOCTYPE html>
@@ -12,14 +26,27 @@ $verlinkungHomepage = 'index.php';
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Auktify | Profil</title>
     <link rel="stylesheet" href="styles/styles.css">
+    <link rel="stylesheet" href="styles/profile.css">
 </head>
 <body>
     <?php require __DIR__ . '/partials/header.php'; ?>
     <main>
         <section class="section">
-            <div class="section-text center">
-                <h1>Mein Profil</h1>
-                <p>Hier können Sie Ihre Profildaten einsehen und bearbeiten.</p>
+            <div class="section-text center profile-container">
+                <h1><strong>Mein Profil</h1>
+                <hr>
+                <h3><strong>Persönliche Informationen</strong></h3>
+                <p><strong>Name: </strong> <?php echo $user_from_db['name']; ?></p>
+                <p><strong>E-Mail: </strong> <?php echo $user_from_db['mail']; ?></p>
+                <hr>
+                <h3><strong>Adresse</strong></h3>
+                <p><strong>Ort: </strong><?php echo $user_from_db['ort'] ?></p>
+                <p><strong>Postleitzahl: </strong><?php echo $user_from_db['plz'] ?></p>
+                <p><strong>Straße: </strong> <?php echo $user_from_db['str'] ?></p>
+            </div>
+            <div class="profile-actions">
+                <button class="btn">Profil bearbeiten</button>
+                <button class="btn btn-danger">Abmelden</button>
             </div>
         </section>
     </main>
