b4393701e22a5642ea9b945ef4eb50768553f689
LucaTristl <luca_tristl@hotmail.com>
2025-10-30 15:13:57 +0100

    Bieten funktioniert jzt

--- php/projekt/php-code/Bid.php
+++ php/projekt/php-code/Bid.php
@@ -27,13 +27,30 @@ class Bid {
     }
 
     public function saveBid(): bool {
-        $query = 'INSERT INTO bids (offer_id, mail, price)
-                  VALUES (:offer_id, :bidder_email, :bid_price)';
+        $is_highest_price = $this->is_highest_price();
+
+        $query = 'INSERT INTO bids (offer_id, mail, price, highest_price)
+                  VALUES (:offer_id, :bidder_email, :bid_price, :highest_price)';
         $stmt = $this->dbconnection->prepare($query);
         $stmt->bindValue(':offer_id', $this->offerId, PDO::PARAM_INT);
         $stmt->bindValue(':bidder_email', $this->bidderEmail, PDO::PARAM_STR);
         $stmt->bindValue(':bid_price', $this->bidderPrice);
-
+        $stmt->bindValue(':highest_price', $is_highest_price, PDO::PARAM_BOOL);
         return $stmt->execute();
+    }
+
+    private function is_highest_price(): bool {
+        $query = 'SELECT MAX(price) AS highest_price FROM bids WHERE offer_id = :offer_id';
+        $stmt = $this->dbconnection->prepare($query);
+        $stmt->bindValue(':offer_id', $this->offerId, PDO::PARAM_INT);
+        $stmt->execute();
+
+        $result = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        if ($result === false || $result['highest_price'] === null) {
+            return true;
+        }
+
+        return $this->bidderPrice > (float)$result['highest_price'];
     }
 }
